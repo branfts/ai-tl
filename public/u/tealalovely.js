e38f8373243e9d01d76c1d2b4b97857f3cf88f7c
branfts <support@june07.com>
2024-07-24 17:16:22 -0700

    Updated by A10x Service

---
+++ public/u/tealalovely.js
@@ -0,0 +1,11 @@
+
+const links = [
+    {
+        "url": "https://www.tiktok.com/@tealalovely?lang=en"
+    },
+    {
+        "url": "https://cash.app/$TealaLovely"
+    }
+];
+const rules = [];
+export { links, rules };

835aa21da2f7da928f1ace72ba0c3e505f94b304
branfts <support@june07.com>
2024-07-27 12:45:21 -0700

    Updated by A10x Service

---
+++ public/u/thebradball.js
@@ -0,0 +1,32 @@
+
+const links = [
+    {
+        "url": "https://open.spotify.com/show/6JjquQ5JFxsZHiTQ0J2h7J?si=cbab9cc1cd6748a4&nd=1&dlsi=5167d7c125184cdd"
+    },
+    {
+        "url": "https://www.google.com/maps/place//data=!4m3!3m2!1s0x872b68240aadf495:0x5fb7f9bbc267fbf9!12e1?source=g.page.default"
+    },
+    {
+        "url": "https://www.youtube.com/c/Liquisdesign"
+    },
+    {
+        "url": "https://www.linkedin.com/company/liquis-digital"
+    },
+    {
+        "url": "https://www.pinterest.com/liquisdigital/"
+    },
+    {
+        "url": "https://www.facebook.com/LiquisDigital/"
+    },
+    {
+        "url": "https://x.com/liquisdigital"
+    },
+    {
+        "url": "https://liquisdigital.com/start/"
+    },
+    {
+        "url": "https://www.tiktok.com/@thebradball?lang=en"
+    }
+];
+const rules = [];
+export { links, rules };

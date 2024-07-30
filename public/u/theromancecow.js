e92c66aab83f5ff8b869e7f358a201c7868a660b
branfts <support@june07.com>
2024-07-29 17:26:27 -0700

    Updated by A10x Service

---
+++ public/u/theromancecow.js
@@ -0,0 +1,12 @@
+
+const links = [
+    { url: 'https://www.facebook.com/groups/787472068091894' },
+{ url: 'https://www.instagram.com/theromancecow/' },
+{ url: 'http://theromancecow.com/' },
+{ url: 'https://www.tiktok.com/@theromancecow' }
+]
+const rules = [
+    
+]
+
+export { links, rules }
